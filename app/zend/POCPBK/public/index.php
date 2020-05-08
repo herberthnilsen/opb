777d1a7c3e9cdfbe7b6a4c55c8fcb6cb7adec54f
hnilsen <hnilsen@HNILSEN-BR.br.oracle.com>
2020-05-08 16:24:16 -0300

    wallet

--- app/zend/POCPBK/public/index.php
+++ app/zend/POCPBK/public/index.php
@@ -16,11 +16,32 @@ set_include_path(implode(PATH_SEPARATOR, array(
 
 /** Zend_Application */
 require_once 'Zend/Application.php';
+require_once 'Zend/Cache.php';
+require_once 'Zend/Db/Table/Abstract.php';
+
+
+// First, set up the Cache
+$frontendOptions = array(
+    'automatic_serialization' => true
+    );
+ 
+$backendOptions  = array(
+    'cache_dir' => realpath(APPLICATION_PATH . '/cache')    );
+ 
+$cache = Zend_Cache::factory('Core',
+                             'File',
+                             $frontendOptions,
+                             $backendOptions);
+ 
+// Next, set the cache to be used with all table objects
+Zend_Db_Table_Abstract::setDefaultMetadataCache($cache);
 
 // Create application, bootstrap, and run
 $application = new Zend_Application(
     APPLICATION_ENV,
     APPLICATION_PATH . '/configs/application.ini'
 );
+
+
 $application->bootstrap()
             ->run();
--- app/zend/POCPBK/public/pdo.php
+++ app/zend/POCPBK/public/pdo.php
@@ -1,21 +1,26 @@
 <?php 
+try{
 
-$db = new PDO("oci:dbname=dbpjb_high;charset=utf8", "atpc_user", "0Teste123#456789");
+    $db = new PDO("oci:dbname=pocbank_high;charset=utf8", "atpc_user", "0Teste123#456789");
+}catch(PDOException $e){
+    echo "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
 
 $qtdRegistros = 10;
 $timestamp = (int) (microtime(true) * 1000);
 
-$result =$db->query('select * from atpc_user.sales  where rownum <= '.$qtdRegistros);
+// $result =$db->query('select * from atpc_user.customers ');
 
-$timestampFinalConsulta = (int) (microtime(true) * 1000);
+// $timestampFinalConsulta = (int) (microtime(true) * 1000);
 
-foreach($result as $row) {
-    echo json_encode($row);
-    echo "<br/>";
-}
-$timestampProcessamentoFinal = (int) (microtime(true) * 1000);
+// foreach($result as $row) {
+//     echo json_encode($row);
+//     echo "<br/>";
+// }
+// $timestampProcessamentoFinal = (int) (microtime(true) * 1000);
 
-echo 'Qtd Registros='.$qtdRegistros.
-'<br/>Timestamp Consulta ='.($timestampFinalConsulta-$timestamp).
-'ms<br/> TimeStamp Final Processamento = '.($timestampProcessamentoFinal-$timestamp).'ms';
-?>
+// echo 'Qtd Registros='.$qtdRegistros.
+// '<br/>Timestamp Consulta ='.($timestampFinalConsulta-$timestamp).
+// 'ms<br/> TimeStamp Final Processamento = '.($timestampProcessamentoFinal-$timestamp).'ms';
+// ?>
